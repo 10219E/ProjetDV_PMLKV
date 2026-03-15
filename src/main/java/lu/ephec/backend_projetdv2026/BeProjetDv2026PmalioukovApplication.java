c4fa777c2b1b9744a0a3197890e0728efadb255e
PM <PSR10219@students.ephec.be>
2026-03-15 19:06:53 +0100

    Init project

---
+++ src/main/java/lu/ephec/backend_projetdv2026/BeProjetDv2026PmalioukovApplication.java
@@ -0,0 +1,13 @@
+package lu.ephec.backend_projetdv2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeProjetDv2026PmalioukovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeProjetDv2026PmalioukovApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lu/ephec/backend_projetdv2026/BeProjetDv2026PmalioukovApplicationTests.java
@@ -0,0 +1,13 @@
+package lu.ephec.backend_projetdv2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeProjetDv2026PmalioukovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
