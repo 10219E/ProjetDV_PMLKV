619ef35e256d6fb5d83373ccd40d44ca1fb91d14
PM <PSR10219@students.ephec.be>
2026-04-30 18:22:56 +0200

    1. added users for DB init 2. Added flag for_all (Boolean) as easier than dupe map public holidays for sites

--- backend/src/main/java/lu/ephec/backend_projetdv2026/controller/SiteController.java
+++ backend/src/main/java/lu/ephec/backend_projetdv2026/controller/SiteController.java
@@ -70,6 +70,7 @@ public class SiteController {
             m.put("siteId", c.getSiteId());
             m.put("closureDate", c.getClosureDate());
             m.put("reason", c.getReason());
+            m.put("isForall", c.getForAll());
             return m;
         }).collect(Collectors.toList());
         return ResponseEntity.ok(simplified);
--- backend/src/main/java/lu/ephec/backend_projetdv2026/services/security/ManualHashGen.java
+++ backend/src/main/java/lu/ephec/backend_projetdv2026/services/security/ManualHashGen.java
@@ -6,11 +6,18 @@ public class ManualHashGen {
     public static void main(String[] args) {
         BCryptPasswordEncoder enc = new BCryptPasswordEncoder();
 
-        System.out.println("A001 -> " + enc.encode("S@dminPML!"));
-        System.out.println("G0001 -> " + enc.encode("VIP@ccess!")); //mchlo@ephec.be
-        System.out.println("L0001 -> " + enc.encode("Invite@Usr!")); //sbernard@ephec.be
-        System.out.println("S0001 -> " + enc.encode("Norm@lS!te")); //jmartin@ephec.be
-        System.out.println("L0005 -> " + enc.encode("Invite@123!")); //invite@me.com
-        System.out.println("L0006 -> " + enc.encode("S@dmin1!")); //sone@ephec.be
+        System.out.println("A001 -> " + enc.encode("S@dminPML1!")); //pmlkv@ephec.be
+        System.out.println("A002 -> " + enc.encode("S@dminRHA1!")); //rhardenne@ephec.be
+        System.out.println("M001 -> " + enc.encode("M@dminFVZ1!")); //vfievez@ephec.be
+        System.out.println("M002 -> " + enc.encode("M@dminOGG1!")); //ogues@ephec.be
+        System.out.println("G0001 -> " + enc.encode("VIP@ccess1!")); //mchlo@ephec.be
+        System.out.println("G0002 -> " + enc.encode("VIP@ccess2!")); //jdupont@ephec.be
+        System.out.println("G0003 -> " + enc.encode("VIP@ccess3!")); //gguy@ephec.be
+        System.out.println("S0001 -> " + enc.encode("Norm@lS!te1")); //cmartin@ephec.be
+        System.out.println("S0002 -> " + enc.encode("Norm@lS!te2")); //adubois@ephec.be
+        System.out.println("S0003 -> " + enc.encode("Norm@lS!te3")); //lvandriesche@ephec.be
+        System.out.println("L0001 -> " + enc.encode("Invite@Usr1!")); //sbernard@ephec.be
+        System.out.println("L0002 -> " + enc.encode("Invite@Usr2!")); //tmara@ephec.be
+        System.out.println("L0003 -> " + enc.encode("Invite@Usr3!")); //amariane@ephec.be
     }
 }
