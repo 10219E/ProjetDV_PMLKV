43a2a9fd405c1243ec88aaa2dd68fc95b6ef9f53
PM <PSR10219@students.ephec.be>
2026-05-08 01:00:53 +0200

    Added Jasypt dependency - encryption configuration to encode DB connection password and updated application properties.

--- backend/src/main/java/lu/ephec/backend_projetdv2026/ProjectApplication.java
+++ backend/src/main/java/lu/ephec/backend_projetdv2026/ProjectApplication.java
@@ -1,11 +1,13 @@
 package lu.ephec.backend_projetdv2026;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableEncryptableProperties
 public class ProjectApplication {
 
     public static void main(String[] args) {
