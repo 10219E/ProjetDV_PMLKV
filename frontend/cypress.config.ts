a547b0b0a76758d6ad4d42258254008a63fdc4da
PM <PSR10219@students.ephec.be>
2026-05-07 21:59:02 +0200

    1. init JUNIT in Cypress config instead of in the test 2. make the test create frontend/cypress/results

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,13 +1,18 @@
+// cypress.config.ts
 import { defineConfig } from "cypress";
 
 export default defineConfig({
   allowCypressEnv: false,
-
   component: {
     devServer: {
       framework: "angular",
       bundler: "webpack",
     },
     specPattern: "**/*.cy.ts",
+    reporter: 'junit',
+    reporterOptions: {
+      mochaFile: 'cypress/results/results-[hash].xml',
+      testsuitesTitle: true,
+    }
   },
 });
